4e33d629970d13c34b49b1d48e15e3f16a001c05
Fabian Ferno <fabianferno@gmail.com>
2022-01-07 21:51:31 +0530

    Import styles

--- client/src/App.js
+++ client/src/App.js
@@ -1,69 +1,70 @@
-import React, { useState,useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import { UserContext } from "./UserContext";
-// import Navbar from './components/Navbar';
-import './App.css';
-import Home from './Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Register from "./Register"
-import Doctor from "./Doctor"
-import Patient from "./Patient"
-import test2 from "./contract/Test2.json"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-let Web3 = require("web3")
-let ContractKit = require("@celo/contractkit")
+import Home from "./Home";
+import Register from "./Register";
+import Doctor from "./Doctor";
+import Patient from "./Patient";
+import test2 from "./contract/Test2.json";
+
+import "./assets/scss/style.scss";
+
+let Web3 = require("web3");
+let ContractKit = require("@celo/contractkit");
 // let BigNumber = require("bignumber.js")
-let erc20Abi = require("./erc20Abi.json")
-
+let erc20Abi = require("./erc20Abi.json");
 
 function App() {
-  
   // const [id, setId] = useState(0)
-  const [address, setAddress] = useState()
-  const [web3, setWeb3] = useState(undefined)
-  const [contract,setContract]=useState([])
-  
-  let kit
-let cUSDcontract
-const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1"
+  const [address, setAddress] = useState();
+  const [web3, setWeb3] = useState(undefined);
+  const [contract, setContract] = useState([]);
+
+  let kit;
+  let cUSDcontract;
+  const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
 
   const connectCeloWallet = async function () {
     if (window.celo) {
       try {
-        await window.celo.enable()
-        const web3 = new Web3(window.celo)
-        kit = ContractKit.newKitFromWeb3(web3)
-        const accounts = await kit.web3.eth.getAccounts()
-        kit.defaultAccount = accounts[0]
-        
+        await window.celo.enable();
+        const web3 = new Web3(window.celo);
+        kit = ContractKit.newKitFromWeb3(web3);
+        const accounts = await kit.web3.eth.getAccounts();
+        kit.defaultAccount = accounts[0];
+
         // const Vault=new kit.web3.eth.Contract(test.abi,"0xecEFEcf20Df6cc4954205Ef211a428eAc8C2A6Ed")
 
-        const contr=new kit.web3.eth.Contract(test2,"0x6499cb27999Ec4a90339f3895a87b3a084392F20")
-        console.log(kit.defaultAccount)
-        setWeb3(web3)
+        const contr = new kit.web3.eth.Contract(
+          test2,
+          "0x6499cb27999Ec4a90339f3895a87b3a084392F20"
+        );
+        console.log(kit.defaultAccount);
+        setWeb3(web3);
         // const i=await contr.methods.addresstoId(kit.defaultAccount).call();
-        setAddress(kit.defaultAccount)
-        setContract(contr)
-        cUSDcontract = new kit.web3.eth.Contract(erc20Abi, cUSDContractAddress)
+        setAddress(kit.defaultAccount);
+        setContract(contr);
+        cUSDcontract = new kit.web3.eth.Contract(erc20Abi, cUSDContractAddress);
       } catch (error) {
-        console.log(`⚠️ ${error}.`)
+        console.log(`⚠️ ${error}.`);
       }
     } else {
-      console.log("⚠️ Please install the CeloExtensionWallet.")
+      console.log("⚠️ Please install the CeloExtensionWallet.");
     }
-  }
+  };
 
-  useEffect(connectCeloWallet, [])
+  useEffect(connectCeloWallet, []);
 
   return (
     <>
       <Router>
         <Switch>
-        <UserContext.Provider value={{address,web3,contract}}>
-          <Route path='/' exact component={Home} />
-          <Route path='/register' exact component={Register} />
-          <Route path='/patient' exact component={Patient} />
-          <Route path='/doctor' exact component={Doctor} />
-          
+          <UserContext.Provider value={{ address, web3, contract }}>
+            <Route path="/" exact component={Home} />
+            <Route path="/register" exact component={Register} />
+            <Route path="/patient" exact component={Patient} />
+            <Route path="/doctor" exact component={Doctor} />
           </UserContext.Provider>
         </Switch>
       </Router>
