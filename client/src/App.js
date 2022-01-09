23b8808b55f78fca28845a9c4812bfc565a0cb0c
Fabian Ferno <fabianferno@gmail.com>
2022-01-09 13:03:30 +0530

    Wrap in useEffect

--- client/src/App.js
+++ client/src/App.js
@@ -25,36 +25,43 @@ function App() {
   let cUSDcontract;
   const cUSDContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
 
-  const connectCeloWallet = async function () {
-    if (window.celo) {
-      try {
-        await window.celo.enable();
-        const web3 = new Web3(window.celo);
-        kit = ContractKit.newKitFromWeb3(web3);
-        const accounts = await kit.web3.eth.getAccounts();
-        kit.defaultAccount = accounts[0];
+  useEffect(() => {
+    const connectCeloWallet = async function () {
+      if (window.celo) {
+        try {
+          await window.celo.enable();
+          const web3 = new Web3(window.celo);
+          kit = ContractKit.newKitFromWeb3(web3);
+          const accounts = await kit.web3.eth.getAccounts();
+          kit.defaultAccount = accounts[0];
 
-        // const Vault=new kit.web3.eth.Contract(test.abi,"0xecEFEcf20Df6cc4954205Ef211a428eAc8C2A6Ed")
+          // const Vault=new kit.web3.eth.Contract(test.abi,"0xecEFEcf20Df6cc4954205Ef211a428eAc8C2A6Ed")
 
-        const contr = new kit.web3.eth.Contract(
-          test2,
-          "0xaAc86611a1AF8cFf09a0b8074fa429dA58D5Fe0C"
-        );
-        console.log(kit.defaultAccount);
-        setWeb3(web3);
-        // const i=await contr.methods.addresstoId(kit.defaultAccount).call();
-        setAddress(kit.defaultAccount);
-        setContract(contr);
-        cUSDcontract = new kit.web3.eth.Contract(erc20Abi, cUSDContractAddress);
-      } catch (error) {
-        console.log(`⚠️ ${error}.`);
+          const contr = new kit.web3.eth.Contract(
+            test2,
+            "0xaAc86611a1AF8cFf09a0b8074fa429dA58D5Fe0C"
+          );
+          console.log(kit.defaultAccount);
+          setWeb3(web3);
+          // const i=await contr.methods.addresstoId(kit.defaultAccount).call();
+          setAddress(kit.defaultAccount);
+          setContract(contr);
+          cUSDcontract = new kit.web3.eth.Contract(
+            erc20Abi,
+            cUSDContractAddress
+          );
+
+          console.log(cUSDcontract);
+        } catch (error) {
+          console.log(`⚠️ ${error}.`);
+        }
+      } else {
+        console.log("⚠️ Please install the CeloExtensionWallet.");
       }
-    } else {
-      console.log("⚠️ Please install the CeloExtensionWallet.");
-    }
-  };
+    };
 
-  useEffect(connectCeloWallet, []);
+    connectCeloWallet();
+  }, []);
 
   return (
     <>
